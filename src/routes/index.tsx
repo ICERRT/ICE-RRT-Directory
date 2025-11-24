3c9fcdc94c5a8438f0e9b0b37b801efd2d2ffa30
John Duncan <jduncan@equitytech.consulting>
2025-11-24 23:10:50 +0000

    redirect to dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
 import { Card } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 
-export const Route = createFileRoute('/')({ ssr: false, component: App })
+export const Route = createFileRoute('/')({ 
+  ssr: false, 
+  component: App,
+  loader: () => { throw redirect({ to: '/dashboard'})} 
+})
 
 function App() {
   return (
